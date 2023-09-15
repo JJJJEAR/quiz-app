b48e6f72cc47540b4318ad697dcbd3fa5b7f1711
JJJJEAR <110074209+JJJJEAR@users.noreply.github.com>
2023-09-15 13:58:57 +0700

    Add Menu.js Quiz.js Score.js
    Add Menu.js Quiz.js Score.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import Menu from "./component/Menu";
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
+
+import './App.css'
+
+export const DataContext = createContext() 
 
 function App() {
+  const [appState, setAppState] = useState("menu")
   return (
+  <>
+    <DataContext.Provider value={{appState,setAppState}}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Web Development quiz</h1>
+      {appState === "menu" && <Menu/>}
+      {appState === "quiz" && <Quiz/>}
+      {appState === "score" && <Score/>}
     </div>
+  </DataContext.Provider>
+  </>
   );
 }
 
---
+++ src/data/QuestionData.js
@@ -0,0 +1,28 @@
+const QuestionData = [
+    {
+        question:"ข้อใดไม่ใช่ tag html",
+        A:"<react>",
+        D:"<a>",
+        C:"<p>",
+        D:"<h1>",
+        answer:"A", 
+    },
+    {
+        question:"ข้อใดไม่ถูกต้องเกี่ยวกับ React",
+        A:"ทำงานฝั่ง Server",
+        D:"ใช้จัดการฐานข้อมูล",
+        C:"เป็น JavaScript Library",
+        D:"ถูกทุกข้อ",
+        answer:"C", 
+    },
+    {
+        question:"ข้อใดคือชื่อ Browser",
+        A:"PHP",
+        D:"React",
+        C:"MySQL",
+        D:"Chrome",
+        answer:"D", 
+    },
+]
+
+export default QuestionData
